688f2abe6832cdfd5794e4993417b5a5d9a50355
gbmao <gabriel.machado.oli@gmail.com>
2025-04-30 11:36:54 -0300

    Primeiro commit: adicionando exercícios de c#

---
+++ README.md
@@ -0,0 +1,26 @@
+# Exercícios de C#
+
+Repositorios  com meus exercicios em c#
+
+tentarei separar em:
+
+*Exercicios Guiados(cursos online)
+
+*Exercicios ChatGPT(pequenos exercicios criados pelo chat)
+
+
+
+
+
+
+
+
+---------------------------
+
+entre no terminal e navegue até a pasta do exercicio
+execute o comando   code .
+o VSCODE ira abrir o arquivo
+
+
+dotnet run ----  executa o arquivo
+ 
---
+++ exerciciosChatGpt/exercicioGpt/Program.cs
@@ -0,0 +1,49 @@
+﻿string[] alunos = { "Ana", "Bruno", "Carlos" };
+int[] notasAna = { 80, 90, 85, 70 };   // 4 notas da Ana
+int[] notasBruno = { 75, 80, 85, 90 }; // 4 notas do Bruno
+int[] notasCarlos = { 90, 85, 88, 80 }; // 4 notas do Carlos
+int numeroMaterias = 4;
+float mediaFinal = 0;
+//int nota = 0;
+
+Console.WriteLine("Aluno\tMédia");
+
+// foreach para percorrer os nomes dos alunos
+foreach (string nome in alunos)
+{
+    int somaNotas = 0;
+    //string alunoAtual = nome;
+    
+    if (nome == "Ana")
+    {
+        foreach (int nota in notasAna)
+        {
+            somaNotas += nota;       
+        }
+
+    }
+
+    if (nome == "Bruno")
+    {
+        foreach (int nota in notasBruno)
+        {
+            somaNotas += nota;       
+        }
+
+
+    }
+
+    if (nome == "Carlos")
+    {
+        foreach (int nota in notasCarlos)
+        {
+            somaNotas += nota;       
+        }
+
+    }
+
+        mediaFinal = (float)somaNotas / numeroMaterias;
+        Console.WriteLine(nome +"\t" + mediaFinal);
+
+    
+}
