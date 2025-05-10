f3a02a757a1ca5b19881d22312b2b54fe07f93c7
gbmao <gabriel.machado.oli@gmail.com>
2025-05-10 11:47:17 -0300

    Primeiro commit do exercicio 1.

---
+++ guidedChallengesDotNEt/ValidandoEntradaDeInt/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Console.WriteLine("Please enter an integer between 5 and 10:");
+string input = Console.ReadLine();
+int number = 0;
+bool intNumber = false;
+intNumber = int.TryParse(input, out number); */
+
+//number++;
+
+//Console.WriteLine(number);
+
+int number = 0;
+do
+{
+    Console.WriteLine("Please enter an integer between 5 and 10:");
+    string? input = Console.ReadLine();
+    
+    bool intNumber = false;
+
+    intNumber = int.TryParse(input, out number); 
+    
+    //A solução deve garantir que a entrada seja uma representação válida de um inteiro.
+    if (intNumber == true) 
+    {
+        
+           //Se o valor inteiro não estiver entre 5 e 10, 
+           // o código deverá usar uma instrução Console.WriteLine() 
+           // para solicitar ao usuário um valor inteiro entre 5 e 10.
+            if ((number <= 5)||(number >= 10))
+            {
+                Console.WriteLine($"You entered {number}. The number must be bewteen 5 and 10...");
+                continue;
+            }
+
+    } else 
+    {
+            Console.WriteLine("Not a valid integer number, please try again");
+            continue;
+    }
+
+} while ((number < 5)||(number > 10));
+
+Console.WriteLine($"Your input value ({number}) has been acepted.");
---
+++ guidedChallengesDotNEt/ValidandoEntradaDeInt/README.md
@@ -0,0 +1,22 @@
+### Validando entrada de INT
+- Desafio do curso learn da microsoft para validar a entrada de int
+
+## REGRAS:
+1.*A solução deve incluir uma iteração do-while ou while.*
+
+2.Antes do bloco de iteração: a solução deve usar uma instrução Console.WriteLine() para solicitar ao usuário um valor inteiro entre 5 e 10.
+
+*Dentro do bloco de iteração:*
+
+3.A solução deve usar uma instrução Console.ReadLine() para obter uma entrada do usuário.
+4.A solução deve garantir que a entrada seja uma representação válida de um inteiro.
+5.Se o valor inteiro não estiver entre 5 e 10, o código deverá usar uma instrução Console.WriteLine() para solicitar ao usuário um valor inteiro entre 5 e 10.
+6.A solução deve garantir que o valor inteiro esteja entre 5 e 10 antes de sair da iteração.
+7.Abaixo (depois) do bloco de código de iteração: a solução deve usar uma instrução Console.WriteLine() para informar ao usuário que o valor de entrada foi aceito.
+
+#Como abrir
+
+1. navegue até a pasta do arquivo no bash
+2. Digite :
+```bash
+code .
